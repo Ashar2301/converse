48a3015367893b31bcad154186ed714d2b24a45c
Ashar Rashid <ashar.rashid2301@gmail.com>
2021-06-14 17:06:14 +0530

    fixed auth

--- src/HomePage/SignUp.js
+++ src/HomePage/SignUp.js
@@ -1,8 +1,14 @@
-import react from 'react';
 import classes from './SignUp.module.css'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import { useState } from 'react'
 const SignUp = (props) =>{
+
+    const [email , setEmail] = useState(true)
+    const [pswd , setPswd] = useState(true)
+    const [cpswd , setCpswd] = useState(true)
+    const [name , setName] = useState(true)
+
     
     let pwd = '';
     const usernameAuth=(event)=>{
@@ -11,9 +17,11 @@ const SignUp = (props) =>{
         const test = pattern.test(event.target.value);
         if(!test){
             elm.style.display = 'block';
+            setEmail(true)
         }
         else{
             elm.style.display = 'none';
+            setEmail(false)
         }
     }
     const passwordAuth=(event)=>{
@@ -23,18 +31,22 @@ const SignUp = (props) =>{
         const test = pattern.test(event.target.value);
         if(!test){
             elm.style.display = 'block';
+            setPswd(true)
         }
         else{
             elm.style.display = 'none';
+            setPswd(false)
         }
     }
     const cPasswordAuth=(event)=>{
         const elm= document.getElementById('passwordAuth3');
-        if(pwd != event.target.value){
+        if(event.target.value != document.getElementById('inputpswd').value){
             elm.style.display = 'block';
+            setCpswd(true)
         }
         else{
             elm.style.display = 'none';
+            setCpswd(false)
         }
     }
     const onSignInClick=()=>{
@@ -53,20 +65,37 @@ const SignUp = (props) =>{
         })
         .catch(err => console.log(err))
     }
+    const onNameChange=(event)=>{
+        if(event.target.value.length > 0){
+            document.getElementById('nameAuth2').style.display = "none"
+            setName(false)
+        }
+        else{
+            document.getElementById('nameAuth2').style.display = "block"
+            setName(true)
+        }
+    }
     return(
         <div className={classes.SignUpSection}>
         <h1>Sign Up</h1>
-        <input className={classes.input} id="input1" placeholder="ENTER NAME"></input>
-        
+        <input className={classes.input} onChange={onNameChange} id="input1" placeholder="ENTER NAME"></input>
+
+        <h4 className={classes.usernameAuth} id="nameAuth2" >Enter Your Name!</h4>
+
         <input className={classes.input} id="input2" onChange={usernameAuth} placeholder="ENTER USERNAME"></input>
+
         <h4 className={classes.usernameAuth} id="usernameAuth2" >Enter A Valid Username [A-Z,a-z,0-9 , between 6-8 letters] !</h4>
-        <input className={classes.input}  onChange={passwordAuth} type = "password" placeholder="ENTER PASSWORD"></input>
+
+        <input className={classes.input} id="inputpswd" onChange={passwordAuth} type = "password" placeholder="ENTER PASSWORD"></input>
+
         <h4 className={classes.passwordAuth} id="passwordAuth2" >Enter A Valid Password [A-Z,a-z,0-9 , atleast 4 letters] !</h4>
+
         <input className={classes.input} id="input3" onChange={cPasswordAuth} type = "password" placeholder="CONFIRM PASSWORD"></input>
+
         <h4 className={classes.passwordAuth2} id="passwordAuth3" >Password Does Not Match !</h4>
         <Link to='/'><button
         //  onClick={()=>props.onCAClick()} 
-         className={classes.SignUpBtn} onClick={onSignInClick}>Sign Up</button></Link>
+         className={classes.SignUpBtn} disabled={name || email || pswd || cpswd} onClick={onSignInClick}>Sign Up</button></Link>
         </div>
     )
 }
